4f10f2276752af4400640e82126aa136c35804d4
Pranav Srigiriraju <pss9179@stern.nyu.edu>
2026-01-08 12:33:22 -0600

    Rename package to llmobserve-sdk for PyPI publishing
    Package name 'llmobserve' was already taken on PyPI.
    Changed to 'llmobserve-sdk' so we can publish with current account.
    
    Users install with: pip install llmobserve-sdk
    Import still: from llmobserve import observe

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -12,7 +12,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
 
 setup(
-    name="llmobserve",
+    name="llmobserve-sdk",
     version="0.3.0",
     author="Pranav Srigiriraju",
     author_email="support@llmobserve.com",
